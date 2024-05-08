96d9d3269b8f0bd4f72464b0593687bd7e2af5c8
choidonghe <fsfsfs1@naver.com>
2024-05-08 12:23:05 +0900

    / 로그인,,,, 회원가입

---
+++ Project_Used_Trade/src/main/java/com/itwillbs/user/UserDAO.java
@@ -0,0 +1,27 @@
+package com.itwillbs.user;
+
+import java.util.List;
+
+import com.itwillbs.user.AuthVO;
+import com.itwillbs.user.UserVO;
+
+public interface UserDAO {
+
+	public void joinBoard(UserVO vo) throws Exception;
+	
+	public void authJoin(AuthVO avo) throws Exception;
+	
+	public UserVO loginBoard(UserVO vo) throws Exception;
+	
+	public int checkIdBoard(String id) throws Exception;
+	
+	public List<UserVO> findIdBoard(UserVO vo) throws Exception;
+	
+	public UserVO findPwBoard(UserVO vo) throws Exception;
+	
+	public void pwUpdate(UserVO vo) throws Exception;
+	
+	public UserVO read(String id) throws Exception;
+	
+	
+}
